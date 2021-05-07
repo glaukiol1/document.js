8f1b6f8f16e40a9b431ebb37ef0f57a84467799b
Glaukio <glaukiol1@gmail.com>
2021-05-07 21:07:16 +0000

    Update script.js for new function names

--- test-files/script.js
+++ test-files/script.js
@@ -2,10 +2,10 @@ import module from '../modules/exports.js';
 
 
 document.getElementById('show').addEventListener('click', () => {
-    module.addDOMElememtNoParams('btn-show', 'h2', 'Test 1');
-    module.h1HeadingWithNoParams('btn-show', 'Test Two')
-    module.h1HeadingWithID('btn-show', 'Test 3', 'first-h1-id')
-    module.h1HeadingWithIDandClass('btn-show', 'Test 4', 'this-is-a-id', 'this-is-a-class')
+    module.newElement('btn-show', 'h2', 'Test 1');
+    module.h1('btn-show', 'Test Two')
+    module.h1WithId('btn-show', 'Test 3', 'first-h1-id')
+    module.h1WithIdAndClass('btn-show', 'Test 4', 'this-is-a-id', 'this-is-a-class')
     module.createDiv(['Glaukio', 'Codes'], ['h1', 'h2'], 'btn-show', 'this-is-another-id');
 });
 
