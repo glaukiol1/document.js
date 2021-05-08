e7537f8b38c6f6fac4651d46bed593d7c1dc8901
glaukiol1 <glaukiol1@gmail.com>
2021-05-08 20:05:42 +0000

    Added deatils to test-files

--- test-files/script.js
+++ test-files/script.js
@@ -7,14 +7,10 @@ document.getElementById('show').addEventListener('click', () => {
     documentjs.h1('btn-show', 'Test 3', 'first-h1-id')
     documentjs.h1('btn-show', 'Test 4', 'this-is-a-id', 'this-is-a-class')
     documentjs.h2('btn-show', 'Test 5', 'this-is-another-id2a', 'this-is-a-class')
+    documentjs.h3('btn-show', 'Test for addId', null, 'test-class')
     documentjs.createDiv(['Glaukio', 'Codes'], ['h1', 'h2'], 'btn-show', 'this-is-another-id');
+    documentjs.addClass(document.getElementById('this-is-a-id'), 'class-test-1', 'class-test-2')
+    documentjs.addId(document.getElementsByClassName('test-class')[0], 'test-id')
 });
-
-// const example = ['h1', 'h2', 'h3']
-
-// for(let i = 0; i < example.length; i++) {
-//     const inArray = example[i];
-//     console.log(inArray);
-// }
 
 // You can write more code to test if you would like
